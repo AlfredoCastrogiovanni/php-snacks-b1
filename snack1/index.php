ba8ae1c60f26bd99a221ee6bc2224cc486ea2856
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-01-15 12:23:40 +0100

    first commit

---
+++ snack1/index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $matches = [
+        [
+            "team1" => "Torino",
+            "team2" => "Bari",
+            "score1" => 10,
+            "score2" => 5
+        ],
+        [
+            "team1" => "Bologna",
+            "team2" => "Genova",
+            "score1" => 20,
+            "score2" => 8
+        ],
+        [
+            "team1" => "Inter",
+            "team2" => "Lecce",
+            "score1" => 11,
+            "score2" => 4
+        ]
+    ];
+?>
+
+<div>
+    <ul>
+        <?php foreach($matches as $match) { ?>
+            <li>
+                <?php
+                    echo $match["team1"] . " - ";
+                    echo $match["team2"] . " | ";
+                    echo $match["score1"] . " - ";
+                    echo $match["score2"] . " ";
+                ?>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
