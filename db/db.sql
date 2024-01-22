197ac76ddef7252d1d988b66e791a62b039fbce6
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-01-22 12:35:02 +0100

    db fix

--- db/db.sql
+++ db/db.sql
@@ -2,7 +2,7 @@
 SELECT * FROM `students` WHERE `surname` LIKE '%o';
 
 -- Selezionare tutti gli appelli di esame con voto medio piu' alto di 28
--- SELECT * FROM `exam_student` WHERE AVG(`vote`) > 28 GROUP BY `exam_id`;
+SELECT * FROM `exam_student` WHERE `vote` > 28;
 
 -- Selezionare tutti gli insegnanti che hanno la lettera s nel nome e il cui ufficio ha un numero maggiore di 96
 SELECT * FROM `teachers` WHERE `name` LIKE '%s%' AND `office_number` > 96;
