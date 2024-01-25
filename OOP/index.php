1ce3d8118067884a499b45850de384ae5ba038e5
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-01-25 12:42:01 +0100

    add category and layout

--- OOP/index.php
+++ OOP/index.php
@@ -1,5 +1,10 @@
 <?php
-    require_once __DIR__ . '/index.php';
+    require_once __DIR__ . '/models/Pokemon.php';
+    require_once __DIR__ . '/models/Category.php';
+
+    $Pokemon1 = new Pokemon('pino', 'maschio', 3, 1.20,20 );
+
+    $Pokemons = [$Pokemon1, $Pokemon1, $Pokemon1 ];
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +13,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- Boostrap CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Pokedex</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
